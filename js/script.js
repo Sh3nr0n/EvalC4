ddef6237162039645b003ce5328a726120150ffe
Sh3nr0n <mehdi.lefeuvre@gmail.com>
2018-03-20 17:12:47 +0100

    almost done

--- js/script.js
+++ js/script.js
@@ -2,19 +2,31 @@ $(document).ready(function () {
     $("#sales").on("click", function (){
         $('#newSale').modal('show');
         $('#sales').attr({
-            src: 'img/shopping-cart.svg'
+            src: 'img/shopping-cart_on.svg'
         });
     });
     $("#addEntry").on("click", function (){
         $('#newEntry').modal('show');
+        $('#addEntry').attr({
+            src: 'img/plus-circle_on.svg'
+        });
     });
     $("#lostStock").on("click", function (){
         $('#losses').modal('show');
+        $('#lostStock').attr({
+            src: 'img/minus-circle_on.svg'
+        });
     });
     $("#printList").on("click", function (){
         $('#print').modal('show');
+        $('#printList').attr({
+            src: 'img/print_on.svg'
+        });
     });
     $("#marketing").on("click", function (){
         $('#cities').modal('show');
+        $('#marketing').attr({
+            src: 'img/map-marker-alt_on.svg'
+        });
     });
 });
